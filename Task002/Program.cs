67e729f97c4a728c3d15b7ca8762b8aa1e85289b
kofas2 <55452453+kofas2@users.noreply.github.com>
2023-03-25 01:39:08 +0300

    add Task002 Seminar 5

---
+++ Task002/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+
+void FillArray(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-9,10);
+    }
+}
+
+void PrintArray(int []array)
+{
+    foreach (int item in array)
+    {
+        Console.Write($"{item} ");
+    }
+    Console.WriteLine();
+
+}
+
+int[] ReverseArray(int [] array)
+{
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i]*=-1;
+    }
+    return array;
+}
+
+
+int [] array = new int [4];
+FillArray(array);
+PrintArray(array);
+ReverseArray(array);
+PrintArray(array);
