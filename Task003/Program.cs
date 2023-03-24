82fd6eed700704f0e0b66083b726b452804091ab
kofas2 <55452453+kofas2@users.noreply.github.com>
2023-03-25 02:19:22 +0300

    add Task003 Seminar 5

---
+++ Task003/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая определяет, 
+// присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+
+void FillArray(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0,20);
+    }
+}
+
+void PrintArray(int []array)
+{
+    foreach (int item in array)
+    {
+        Console.Write($"{item} ");
+    }
+    Console.WriteLine();
+}
+
+string CheckA(int a, int [] array)
+{
+    string temp = String.Empty;
+
+    foreach (int i in array)
+    {
+        if (i == a)
+        {
+            temp = "да";
+            break;
+        }
+        else
+        {
+            temp = "нет";
+        }
+    }
+    return temp;
+
+}
+
+
+Console.WriteLine("Введите количество значений массива");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число, чтобы узнать, присутствет ли оно в массиве");
+int a = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [n];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine($"{CheckA(a, array)}");
